96ae59fce79d393fb7bd37aae52f42118a3341ea
Matt Cheah <matt.cheah@gmail.com>
2018-03-07 10:21:12 -0800

    implement parallax

--- src/app/hero-image/hero-image.component.ts
+++ src/app/hero-image/hero-image.component.ts
@@ -10,6 +10,15 @@ export class HeroImageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll',this.parallax);
+  }
+
+  parallax = ():void => {
+    let scrollPos = window.pageYOffset;
+    if (scrollPos <= 920) {
+      let image = document.getElementById("hero-image-container");
+      image.style.backgroundPositionY = (scrollPos *.4) + "px";
+    }
   }
 
 }
