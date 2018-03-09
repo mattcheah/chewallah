134a5c999a79cf95b8296a5631212b7e78708232
Matt Cheah <matt.cheah@gmail.com>
2018-03-09 09:07:12 -0800

    add slider rotator for ease of editing 3d properties

--- src/app/art-piece/art-piece.component.ts
+++ src/app/art-piece/art-piece.component.ts
@@ -10,9 +10,35 @@ export class ArtPieceComponent implements OnInit {
   @Input() piece: ArtPiece;
   @Input() offset: string;
 
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterContentInit() {
+
+
+    // slider.addEventListener('change', function (e) {
+    //   let rotater = document.querySelector("#" + this.piece.nameCode + " .art-piece-pseudo");
+    //   let value = slider.value;
+    //   console.log(value);
+    // });
+  }
+
+  ngAfterViewInit() {
+    let myPiece = this.piece;
+
+    let slider = document.getElementById('slider-' + myPiece.nameCode);
+    console.log('slider-' + myPiece.nameCode);
+    console.log(slider);
+
+    slider.addEventListener('change', function (e) {
+      let rotater = document.querySelector("#" + myPiece.nameCode + " .art-piece-pseudo");
+      let value = slider.value;
+      rotater.style.transform = "rotateY("+(value-90)+"deg)";
+    });
   }
 
 }
