00c63a15d5377bfbd8767fc22b447d0a8f3ed1f0
Matt Cheah <matt.cheah@gmail.com>
2018-03-12 12:12:48 -0700

    set up router with about page works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { ArtListComponent } from './art-list/art-list.component';
@@ -22,7 +22,8 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,18 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const appRoutes: Routes = [];
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
