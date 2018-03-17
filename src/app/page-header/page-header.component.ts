e57c613f89c71b9fa3b00d281b97dc6b92c79e53
Matt <matt.cheah@gmail.com>
2018-03-17 16:10:46 -0700

    add responsive media queries to portfolio page

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
   @Input() pageTitle: string;
+  @Input() headerImage: string;
 
   constructor() { }
 
